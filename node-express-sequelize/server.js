88efcbd73c5aa88c3390d0612e6aa9216e9244d7
mohamed <toot2009@hotmail.com>
2017-02-04 09:39:46 +0200

    no new

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -12,8 +12,30 @@ var User = sequelize.define('User', {
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
 //if the Qu need to mack reuter this is
-route
+router.get(function (req,res,next){
+	
+	//resev the git req from 'user'
+	//.send to sequli
+	req.send(req.user)
+})
+// app.param('User', function(req, res, next) {
 
+//   // try to get the User details from the User model and attach it to the request object
+//   User.find(function(err, User) {
+//     if (err) {
+//       next(err);
+//     } else if (User) {
+//       req.User = User;
+//       next();
+//     } else {
+//       next(new Error('failed to load User'));
+//     }
+//   });
+// })
+
+// router.use(function(req,res,next){
+
+// })
 
 module.exports = { 
   app: app,
